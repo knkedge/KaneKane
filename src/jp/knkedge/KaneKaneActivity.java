b10459239963d276c215f0e909a0f6d9d2547f0d
Kanako Watanabe <crossedge.knk@gmail.com>
2012-05-15 09:42:24 +0900

    Add New Android Project

---
+++ src/jp/knkedge/KaneKaneActivity.java
@@ -0,0 +1,13 @@
+package jp.knkedge;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class KaneKaneActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
